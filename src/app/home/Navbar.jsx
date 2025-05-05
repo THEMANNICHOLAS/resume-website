4ba5c0bd9f94b22abca8ea949187d9f7cc165e32
THEMANNICHOLAS <stingyblue@gmail.com>
2025-05-05 13:04:07 -0400

    Add mobile menu changes

--- src/app/home/Navbar.jsx
+++ src/app/home/Navbar.jsx
@@ -1,8 +1,17 @@
-import React from "react";
+import React, { useRef } from "react";
 import { assets } from "../../../assets/assets.js";
 import Image from "next/image.js";
 
 const Navbar = () => {
+
+    const sideMenuRef = useRef();
+    const openMenu = ()=>{
+        sideMenuRef.current.style.transform = 'translateX(-16rem)';
+    }
+    const closeMenu = ()=>{
+        sideMenuRef.current.style.transform = 'translateX(16rem)';
+    }
+
     return (
         <>
         <div className="fixed top-0 left-0 w-full h-screen -z-10">
@@ -32,21 +41,21 @@ const Navbar = () => {
                     <Image src={assets.images.contact_logo} alt='email' className='w-3'/>
                     </a>
 
-                    <button className="block md:hidden ml-4">
+                    <button className="block md:hidden ml-4" onClick = {openMenu}>
                         <Image src={assets.images.menuIcon} alt='null' className='w-8 h-8'/>
                     </button>
 
                 </div>
                 {/*---------------Mobile Menu-------------------*/}
-                <ul className="flex md:hidden flex-col gap-4 py-20 px-10 fixed -right-0
+                <ul ref ={sideMenuRef} className="flex md:hidden flex-col gap-4 py-20 px-10 fixed -right-64
                 top-0 bottom-0 w-64 z-50 h-screen bg-[#E0F7FF] transition duration-500">
-                    <div className='absolute right-6 top-6'>
+                    <div className='absolute right-6 top-6' onClick ={closeMenu}>
                         <Image src={assets.images.crossIcon} alt='null' className='w-5 cursor-pointer'/>
                     </div>
-                    <li><a href="#top" className="transition-colors duration-200 hover:text-blue-400">Home</a></li>
-                    <li><a href="#about" className="transition-colors duration-200 hover:text-blue-400">About Me</a></li>
-                    <li><a href="#work" className="transition-colors duration-200 hover:text-blue-400">My Projects</a></li> 
-                    <li><a href="#contact" className="transition-colors duration-200 hover:text-blue-400">Contact Me</a></li>
+                    <li><a href="#top" onClick ={closeMenu} className="transition-colors duration-200 hover:text-blue-400">Home</a></li>
+                    <li><a href="#about" onClick ={closeMenu} className="transition-colors duration-200 hover:text-blue-400">About Me</a></li>
+                    <li><a href="#work" onClick ={closeMenu} className="transition-colors duration-200 hover:text-blue-400">My Projects</a></li> 
+                    <li><a href="#contact" onClick ={closeMenu} className="transition-colors duration-200 hover:text-blue-400">Contact Me</a></li>
                 </ul>
             </nav>
         </>

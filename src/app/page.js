946baf30fb0fd4176d20d12c2324d821d7bc08c2
THEMANNICHOLAS <stingyblue@gmail.com>
2025-06-03 12:57:25 -0400

    New changes

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,16 @@
 'use client'
 import Image from "next/image";
 import Navbar from "./home/Navbar";
+import Social from "./home/Social";
 
 
 export default function Home() {
   return (
     <>
-    <Navbar/>
-
+      <Navbar/>
+      
+      <Social/>
     </>
-      );
+  );
 }
+

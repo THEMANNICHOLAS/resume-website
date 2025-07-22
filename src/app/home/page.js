9f675f7e8a8cb9c4db7f3f220b7e6410d9dd973c
THEMANNICHOLAS <stingyblue@gmail.com>
2025-07-22 12:08:02 -0400

    Add home page ui layout

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -1,10 +1,17 @@
+'use client'
+import Image from "next/image";
+import Navbar from "./components/Navbar";
+import Social from "./components/Social";
+import InfoCard from "./components/InfoCard";
 
 
 export default function Home() {
   return (
     <>
-      
-      
+    
+      <main className="pt-20"></main>
+      <InfoCard title='Welcome'/>
     </>
-  )
+  );
 }
+
